faba3819034214c0c85db3fcf1f4076968b0b93f
Vadkon <vadkon2007@gmail.com>
2024-11-26 08:35:23 -0500

    Update 0.2.0

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import ffmpeg
+import qdarkstyle
 
 from PyQt6.QtGui import QAction, QIcon, QPixmap
 from PyQt6.QtWidgets import (
@@ -21,21 +22,27 @@ from PyQt6.QtWidgets import (
 )
 from PyQt6.QtCore import Qt
 
+app_version = '0.2.0'
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("ImgFormat")
-        self.setGeometry(100, 100, 600, 400)
+        self.setWindowTitle(f"ImgFormat {app_version}")
+        self.setGeometry(100, 100, 400, 350)
 
         # Set up central widget and layout
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
         self.layout = QVBoxLayout(self.central_widget)
 
-        # Create and add QLabel
-        self.label = QLabel('Please, choose a file to convert')
-        self.layout.addWidget(self.label, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.widgetF = QLabel("Please, choose a file to convert in 'File' menu")
+        widget_font = self.widgetF.font()
+        widget_font.setPointSize(16)
+        widget_font.setBold(True)
+        self.widgetF.setFont(widget_font)
+        self.widgetF.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+        self.layout.addWidget(self.widgetF)
 
         # Create Menu Bar
         menu_bar = self.menuBar()
@@ -50,6 +57,8 @@ class MainWindow(QMainWindow):
         exit_action.triggered.connect(self.close)
         file_menu.addAction(exit_action)
 
+        #self.check_updates()
+
         # Create Status Bar
         self.statusBar().showMessage('Ready')
 
@@ -58,7 +67,7 @@ class MainWindow(QMainWindow):
         if self.path:
             self.statusBar().showMessage(f'Selected file: {self.path}')
             print(self.path)
-            self.label.hide()
+            self.widgetF.hide()
             self.show_image(self.path)
 
     def show_image(self, path):
@@ -76,6 +85,8 @@ class MainWindow(QMainWindow):
 
         self.layout.addWidget(image_label, alignment=Qt.AlignmentFlag.AlignCenter)
 
+        # Input form and suggestions for her. Note that often special formats (raw, heif, etc.) don't work with some formats
+
         self.input = QLineEdit()
         self.input.setPlaceholderText("Enter format...")
 
@@ -83,17 +94,37 @@ class MainWindow(QMainWindow):
         completer = QCompleter(suggestions)
         self.input.setCompleter(completer)
 
-        self.input.setFixedWidth(100)
+        self.input.setFixedWidth(150)
         self.layout.addWidget(self.input, alignment=Qt.AlignmentFlag.AlignCenter)
 
         # Add Start button
         button = QPushButton("Start", self)
+        button.setFixedWidth(50)
         button.clicked.connect(self.convert_image)
         self.layout.addWidget(button, alignment=Qt.AlignmentFlag.AlignCenter)
 
         # Adjust window size if needed
         if scaled_pixmap.width() > self.width() or scaled_pixmap.height() > self.height():
             self.resize(scaled_pixmap.width() + 20, scaled_pixmap.height() + 60)
+
+    def check_updates(self):
+        try:
+            url_fetch = 'https://raw.githubusercontent.com/Vadkon07/Splash/refs/heads/master/ver.html'
+            current_version = app_version
+
+            lines_with_word = self.fetch_lines_with_word(url_fetch, current_version)
+            new_version = self.fetch_new_version(url_fetch)
+
+            if not lines_with_word:
+                self.new_update_notif(new_version)
+
+        except Exception as e:
+            dev_mode = 1
+            QMessageBox.warning(self, "Error", f"Please, check your internet connection:\n\n\n {e}.")
+            if dev_mode == 0: # Close app if dev mode is disabled
+                exit()
+            else: # Don't close app if dev mode is enabled
+                print("DEV mode, skip error")
 
     def convert_image(self):
         chosen_format = self.input.text()
@@ -114,6 +145,49 @@ class MainWindow(QMainWindow):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setApplicationName("ImgFormat")
+
+    custom_stylesheet_black = """
+    QWidget {
+        background-color: #1a1a1a;
+        color: white;
+    }
+    QMenuBar {
+        background-color: #1a1a1a;
+        color: white;
+    }
+    QMenuBar::item {
+        background-color: #1a1a1a;
+    }
+    QMenuBar::item:selected {
+        background: #697565;
+        color: white;
+    }
+    QPushButton {
+        background-color: #97fff5;
+        color: black;
+    }
+    QPushButton:hover {
+        background-color: #00d4ff;
+        color: black;
+    }
+    QMenu {
+        background-color: #3C3D37;
+        color: white;
+    }
+    QMenu::item {
+        background-color: #3C3D37;
+        color: white;
+    }
+    QProgressBar {
+        border: 2px solid grey;
+        text-align: center;
+    }
+    QProgressBar::chunk {
+        background-color: #97fff5;
+    }
+   """
+
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt6() + custom_stylesheet_black)
 
     window = MainWindow()
     window.show()
